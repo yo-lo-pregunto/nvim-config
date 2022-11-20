73c6d69f5f9a3a0e552a64ee30f1d2ab941dc8ff
yo-lo-pregunto <junjo.mendoza11@gmail.com>
2022-11-20 15:30:02 -0600

    Floater changes

--- lua/config/floaterm.lua
+++ lua/config/floaterm.lua
@@ -4,6 +4,6 @@ vim.g.floaterm_wintype = 'split'
 vim.g.floaterm_giteditor = true
 vim.g.floaterm_autohide = 1
 vim.g.floaterm_position = 'botright'
-vim.g.floaterm_opener = 'edit'
+vim.g.floaterm_opener = 'split'
 vim.g.floaterm_autoinsert = false
 
