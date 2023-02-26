a9a8ca1fd5a3dab8d571d6467edcf98665cf7d82
yo-lo-pregunto <junjo.mendoza11@gmail.com>
2023-02-26 01:59:20 -0600

    gitsigns added

--- lua/yo-lo-pregunto/packer.lua
+++ lua/yo-lo-pregunto/packer.lua
@@ -4,45 +4,51 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -- Telescope
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  -- Themes
-  use ({
-	  "EdenEast/nightfox.nvim",
-	  config = function()
-		vim.cmd('colorscheme nightfox')
-	end
-  })
-  -- Treesiter / Syntax highliter
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate' })
-  use('nvim-treesitter/playground')
+    -- Themes
+    use ({
+        "EdenEast/nightfox.nvim",
+        config = function()
+            vim.cmd('colorscheme nightfox')
+        end
+    })
+    -- Treesiter / Syntax highliter
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
 
-  -- Harpoon
-  use('ThePrimeagen/harpoon')
+    -- Harpoon
+    use('ThePrimeagen/harpoon')
 
-  -- LSP
-  use {
-	  "williamboman/mason.nvim",
-	  "williamboman/mason-lspconfig.nvim",
-	  "neovim/nvim-lspconfig",
-  }
+    -- LSP
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
 
-  -- AutoCompletion
-  use({'hrsh7th/nvim-cmp'})
-  use({'hrsh7th/cmp-nvim-lsp'})
-  use({'hrsh7th/cmp-buffer'})
-  use({'hrsh7th/cmp-path'})
-  use({'saadparwaiz1/cmp_luasnip'})
-  use({'hrsh7th/cmp-nvim-lua'})
+    -- AutoCompletion
+    use({'hrsh7th/nvim-cmp'})
+    use({'hrsh7th/cmp-nvim-lsp'})
+    use({'hrsh7th/cmp-buffer'})
+    use({'hrsh7th/cmp-path'})
+    use({'saadparwaiz1/cmp_luasnip'})
+    use({'hrsh7th/cmp-nvim-lua'})
 
-      -- Snippets
-  use({'L3MON4D3/LuaSnip'})            -- Required
+    -- Snippets
+    use({'L3MON4D3/LuaSnip'})            -- Required
+
+    -- Gitsigns
+    use {
+        'lewis6991/gitsigns.nvim',
+        tag = 'release'
+    }
 end)
