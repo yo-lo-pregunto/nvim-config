08d2be45c1c3e6b9aa8f5de6d633f7c1b9341590
yo-lo-pregunto <junjo.mendoza11@gmail.com>
2023-03-01 21:53:35 -0600

    Fix some requires

--- init.lua
+++ init.lua
@@ -1,3 +1,1 @@
-require("yo-lo-pregunto.tmux")
-require("yo-lo-pregunto.set")
-require("yo-lo-pregunto.remap")
+require("yo-lo-pregunto")
---
+++ lua/yo-lo-pregunto/init.lua
@@ -0,0 +1,4 @@
+require("yo-lo-pregunto.packer")
+require("yo-lo-pregunto.tmux")
+require("yo-lo-pregunto.set")
+require("yo-lo-pregunto.remap")
