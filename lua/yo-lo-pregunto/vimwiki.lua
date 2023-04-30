a69deccec1b507685708a918a436f2773e7f3e7d
yo-lo-pregunto <junjo.mendoza11@gmail.com>
2023-04-29 20:12:45 -0600

    Fix vimwiki setup
    Signed-off-by: yo-lo-pregunto <junjo.mendoza11@gmail.com>

--- lua/yo-lo-pregunto/vimwiki.lua
+++ lua/yo-lo-pregunto/vimwiki.lua
@@ -1,11 +1,6 @@
-local wiki = {
-    path = "~/vimwiki",
-    nested_syntaxes = {
-        python = "python",
-        ['c++'] = 'cpp',
-        c = 'c',
-        rust = 'rs'
-    }
-}
-
-vim.g.vimwiki_list = wiki
+vim.cmd([[
+    let wiki = {}
+    let wiki.path = '~/vimwiki'
+    let wiki.nested_syntaxes = {'python': 'python', 'c++': 'cpp', 'c': 'c', 'rust': 'rust' }
+    let g:vimwiki_list = [wiki]
+]])
