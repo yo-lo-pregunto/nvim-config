1c1c166748a79d63564580113dde0be9112aff24
yo-lo-pregunto <junjo.mendoza11@iteso.mx>
2023-12-17 19:44:01 -0600

    Added neorg and waiting for dropbar

---
+++ lua/plugins/dropbar.lua
@@ -0,0 +1,9 @@
+-- One day it will work but not for now (required nvim 0.10.0-dev) which I not
+-- going to installed.
+return {
+    -- 'Bekaboo/dropbar.nvim',
+    -- -- optional, but required for fuzzy finder support
+    -- dependencies = {
+    --     'nvim-telescope/telescope-fzf-native.nvim'
+    -- }
+}
---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,36 @@
+return {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        require("neorg").setup {
+            load = {
+                ["core.defaults"] = {}, -- Loads default behaviour
+                ["core.concealer"] = {
+                    config = {
+                        icons_preset = "diamond",
+                    },
+                }, -- Adds pretty icons to your documents
+                ["core.dirman"] = { -- Manages Neorg workspaces
+                    config = {
+                        workspaces = {
+                            notes = "~/Neorg/notes",
+                        },
+                        default_workspace = "notes",
+                    },
+                    ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+                    ["core.integrations.nvim-cmp"] = {},
+                },
+                ["core.summary"] = {},
+                ["core.journal"] = {
+                    config = {
+                        journal_folder = "../journal",
+                        strategy = "flat",
+                    }
+
+                },
+                ["core.esupports.metagen"] = {},
+        },
+    }
+end,
+}
